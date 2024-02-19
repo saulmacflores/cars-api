eeaa2aebcdcbdf51fc7773b66beca236c8e385ab
Eric w <none>
2024-02-19 14:38:21 -0800

    updated rest files and added restfiles folder

--- routesFolder/parts.js
+++ routesFolder/parts.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const studentsController = require('../controllersFolder/parts');
-//const validate = require('../middleware/validate');
-const { isAuthenticated } = require('../middleware/authenticate');
+const partsController = require('../controllersFolder/parts');
+const { isAuthenticated } = require('../middlewareFolder/authenticate');
 const validator = require('../middlewareFolder/validation/validate.parts');
 
 router.get('/', partsController.getAll);
