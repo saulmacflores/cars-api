cffb0c1b05507b0cfe20fceca6d5dd4eb6a4ca9c
khayes133 <kjhayes9@gmail.com>
2024-02-21 18:20:10 -0700

    Fixed User issue

--- controllersFolder/users.js
+++ controllersFolder/users.js
@@ -56,21 +56,27 @@ const createUser = async (req, res) => {
         name: 'required|string',
         avatarImg: 'required|string',
     };
-
-    mongodb
-        .getDb()
-        .collection('users')
-        .insertOne(user, (insertErr, response) => {
-            if (insertErr) {
-                res.status(500).json({
-                    message: 'Some error occurred while creating a user',
-                    error: insertErr,
-                });
-            } else {
-                res.status(201).send();
-            }
+    try {
+        constId = new ObjectId(req.params.id);
+        const result = await mongodb
+            .getDb()
+            .collection('users')
+            .insertOne(user, (insertErr, response)=>{
+                if (result.length > 0) {
+                    res.setHeader('Content-Type', 'application/json');
+                    res.status(200).json(result[0]);
+                } else {
+                    res.status(404).json({ message: 'User not found' });
+                }
+            });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error getting user',
+            error: error.message,
         });
+    }
 };
+   
 
 const updateUser = async (req, res) => {
     if (!ObjectId.isValid(req.params.id)) {
